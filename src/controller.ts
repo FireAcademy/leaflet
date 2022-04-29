54869db0e39aa3d9754f4913aeb591a6836e4e14
yakuhito <y@kuhi.to>
2022-04-29 21:22:48 +0300

    remove logging from recordUsageInDb

--- src/controller.ts
+++ src/controller.ts
@@ -140,7 +140,7 @@ export class Controller {
       date: firestore.FieldValue.serverTimestamp(),
       billed: false,
     };
-    console.log({apiKey, bytes});
+
     await this.db.collection('usage').doc().create(docData);
   }
 
